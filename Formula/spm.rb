8adf68b1c884405c9b11b187f2a2e3734c686732
Dave Redfern <david.redfern@madagasgar.com>
2019-12-16 13:27:40 -0500

    Update release

--- Formula/spm.rb
+++ Formula/spm.rb
@@ -1,8 +1,8 @@
 class Spm < Formula
   desc "Somnambulist Project Manager (SPM) for managing PHP micro-services projects"
   homepage "https://github.com/dave-redfern/somnambulist-project-manager"
-  url "https://github.com/dave-redfern/somnambulist-project-manager/releases/download/0.2.0/somnambulist-project-manager.phar"
-  sha256 "983e168bb8b14519a5e8b0ab5a1dc2e6671d570130d767d7f934a404ae7bc08e"
+  url "https://github.com/dave-redfern/somnambulist-project-manager/releases/download/0.3.0/somnambulist-project-manager.phar"
+  sha256 "61dece0fcc98c107cbd8c43e5a2a90e2f256f1c9f7c3f36f710b009b483e1732"
 
   bottle :unneeded
 
