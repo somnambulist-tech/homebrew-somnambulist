3f959bbfa74e93368065323535407e9478fdc20e
Dave Redfern <david.redfern@madagasgar.com>
2019-12-18 13:51:45 -0500

    Update release

--- Formula/syncit.rb
+++ Formula/syncit.rb
@@ -1,8 +1,8 @@
 class Syncit < Formula
   desc "SyncIt for Mutagen a PHP helper for managing mutagen sessions"
   homepage "https://github.com/dave-redfern/somnambulist-sync-it"
-  url "https://github.com/dave-redfern/somnambulist-sync-it/releases/download/1.0.0/mutagen-sync-it.phar"
-  sha256 "47b2a36ed1c2c5e13801cceec4d80cb4c9861a463cc022913d31f87aa660ca36"
+  url "https://github.com/dave-redfern/somnambulist-sync-it/releases/download/1.0.1/mutagen-sync-it.phar"
+  sha256 "803a0adf52e054f154a7b4d266d0ecd06c64c9ee067a4e24f40fcb61e9110fbb"
 
   bottle :unneeded
 
