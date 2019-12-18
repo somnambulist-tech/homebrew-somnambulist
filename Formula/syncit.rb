607308f00993394f5181c6206ef1ac307d83464a
Dave Redfern <david.redfern@madagasgar.com>
2019-12-18 14:00:24 -0500

    Explicitly set version

--- Formula/syncit.rb
+++ Formula/syncit.rb
@@ -3,6 +3,7 @@ class Syncit < Formula
   homepage "https://github.com/dave-redfern/somnambulist-sync-it"
   url "https://github.com/dave-redfern/somnambulist-sync-it/releases/download/1.0.1/mutagen-sync-it.phar"
   sha256 "803a0adf52e054f154a7b4d266d0ecd06c64c9ee067a4e24f40fcb61e9110fbb"
+  version "1.0.1"
 
   bottle :unneeded
 
@@ -13,6 +14,6 @@ class Syncit < Formula
   end
 
   test do
-    assert_match /^Sync-It with Mutagen 1.0.0$/, shell_output("syncit --version")
+    assert_match /^Sync-It with Mutagen 1.0.1$/, shell_output("syncit --version")
   end
 end
