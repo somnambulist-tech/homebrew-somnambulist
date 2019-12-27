0671f24504adb8c05ded1a0b315d99c7e0047dfb
Dave Redfern <dave-redfern@users.noreply.github.com>
2019-12-27 19:03:23 +0000

    Updating brew formulas

--- Formula/spm.rb
+++ Formula/spm.rb
@@ -2,9 +2,9 @@
 class Spm < Formula
   desc "Somnambulist Project Manager (SPM) for managing PHP micro-services projects"
   homepage "https://github.com/dave-redfern/somnambulist-project-manager"
-  url "https://github.com/dave-redfern/somnambulist-project-manager/releases/download/0.5.3/somnambulist-project-manager.phar"
-  sha256 "19918875032dcffd092329a74209baf68043effeb1d7c0c8c799148cd7aa9071"
-  version "0.5.3"
+  url "https://github.com/dave-redfern/somnambulist-project-manager/releases/download/0.5.4/somnambulist-project-manager.phar"
+  sha256 "122caaf224d1ea0de5e54a9a8bbe64433b31319c74a9b2d90cad94445b981f41"
+  version "0.5.4"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Spm < Formula
   end
 
   test do
-    assert_match /^Somnambulist Project Manager 0.5.3$/, shell_output("spm --version")
+    assert_match /^Somnambulist Project Manager 0.5.4$/, shell_output("spm --version")
   end
 end
 
