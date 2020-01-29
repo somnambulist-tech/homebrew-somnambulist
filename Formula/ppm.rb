c3fd08782fef4b09e362e0fbab24aec138d2cdc3
Dave Redfern <dave-redfern@users.noreply.github.com>
2020-01-29 21:03:57 +0000

    Updating brew formulas

--- Formula/ppm.rb
+++ Formula/ppm.rb
@@ -2,9 +2,9 @@
 class Ppm < Formula
   desc "PHP-PM Process Manager for PHP as a Phar archive"
   homepage "https://github.com/dave-redfern/somnambulist-phppm-phar"
-  url "https://github.com/dave-redfern/somnambulist-phppm-phar/releases/download/3.0.1/ppm.phar"
-  sha256 "93e35140992a15fe0395e4b4983ba6f03b25cdfd3f56454d0660d21d3e314c3c"
-  version "3.0.1"
+  url "https://github.com/dave-redfern/somnambulist-phppm-phar/releases/download/3.0.2/ppm.phar"
+  sha256 "29df322c0b29fbdf09fcaf0fc59e36cae5c0ab302ab138d00ccc3b2f893bd4f1"
+  version "3.0.2"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Ppm < Formula
   end
 
   test do
-    assert_match /^PHP-PM 3.0.1$/, shell_output("ppm --version")
+    assert_match /^PHP-PM 3.0.2$/, shell_output("ppm --version")
   end
 end
 
