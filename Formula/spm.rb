4915b1c86d3bc9d9331cf31a6dd7e9f115161292
Dave Redfern <dave-redfern@users.noreply.github.com>
2020-02-03 17:03:23 +0000

    Updating brew formulas

--- Formula/spm.rb
+++ Formula/spm.rb
@@ -2,7 +2,7 @@
 class Spm < Formula
   desc "Somnambulist Project Manager (SPM) for managing PHP micro-services projects"
   homepage "https://github.com/dave-redfern/somnambulist-project-manager"
-  url "https://github.com/dave-redfern/somnambulist-project-manager/releases/download/0.8.3/somnambulist-project-manager.phar"
+  url "https://github.com/somnambulist-tech/project-manager/releases/download/0.8.3/somnambulist-project-manager.phar"
   sha256 "8efcc45796d5d7eaee25baa4d4e969f86a8f0980287001e4b2f1cf520e099ff5"
   version "0.8.3"
 
--- Formula/syncit.rb
+++ Formula/syncit.rb
@@ -2,7 +2,7 @@
 class Syncit < Formula
   desc "SyncIt for Mutagen a PHP helper for managing mutagen sessions"
   homepage "https://github.com/dave-redfern/somnambulist-sync-it"
-  url "https://github.com/dave-redfern/somnambulist-sync-it/releases/download/1.0.2/mutagen-sync-it.phar"
+  url "https://github.com/somnambulist-tech/sync-it/releases/download/1.0.2/mutagen-sync-it.phar"
   sha256 "d808590413e35378e4f3c66ef96b1c520da7461b099691e5fea13ff4be06364e"
   version "1.0.2"
 
