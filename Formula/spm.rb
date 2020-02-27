9ef65aa110fdbdf68436c4e4c93da94c1c364cf7
Dave Redfern <dave-redfern@users.noreply.github.com>
2020-02-27 20:06:36 +0000

    Updating brew formulas

--- Formula/spm.rb
+++ Formula/spm.rb
@@ -2,9 +2,9 @@
 class Spm < Formula
   desc "Somnambulist Project Manager (SPM) for managing PHP micro-services projects"
   homepage "https://github.com/somnambulist-tech/project-manager"
-  url "https://github.com/somnambulist-tech/project-manager/releases/download/0.10.2/somnambulist-project-manager.phar"
-  sha256 "8930720bfae0e155dceda48b06e41e60bd47e20de456a2b3d8eb18c45dd72f3c"
-  version "0.10.2"
+  url "https://github.com/somnambulist-tech/project-manager/releases/download/0.11.0/somnambulist-project-manager.phar"
+  sha256 "6d4d97854650e3cfed6e423ad3c4eed7b2f85377fbd8d81ec4c0b2a33b794f3e"
+  version "0.11.0"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Spm < Formula
   end
 
   test do
-    assert_match /^Somnambulist Project Manager 0.10.2$/, shell_output("spm --version")
+    assert_match /^Somnambulist Project Manager 0.11.0$/, shell_output("spm --version")
   end
 end
 
--- Formula/syncit.rb
+++ Formula/syncit.rb
@@ -2,9 +2,9 @@
 class Syncit < Formula
   desc "SyncIt for Mutagen a PHP helper for managing mutagen sessions"
   homepage "https://github.com/somnambulist-tech/sync-it"
-  url "https://github.com/somnambulist-tech/sync-it/releases/download/1.0.2/mutagen-sync-it.phar"
-  sha256 "d808590413e35378e4f3c66ef96b1c520da7461b099691e5fea13ff4be06364e"
-  version "1.0.2"
+  url "https://github.com/somnambulist-tech/sync-it/releases/download/1.1.0/mutagen-sync-it.phar"
+  sha256 "109af44c3a96e03a3106ef456874b5999357728001b34f8d73b8e505bcffe16d"
+  version "1.1.0"
 
   bottle :unneeded
 
@@ -15,7 +15,7 @@ class Syncit < Formula
   end
 
   test do
-    assert_match /^Sync-It with Mutagen 1.0.2$/, shell_output("syncit --version")
+    assert_match /^Sync-It with Mutagen 1.1.0$/, shell_output("syncit --version")
   end
 end
 
