004db78401081aded55514fb780d253722f1196c
Dave Redfern <david.redfern@madagasgar.com>
2020-05-27 13:22:16 -0400

    Testing creating symlink from ppm.phar to ppm

--- Formula/ppm.rb
+++ Formula/ppm.rb
@@ -5,13 +5,12 @@ class Ppm < Formula
   url "https://github.com/somnambulist-tech/phppm-phar/releases/download/4.2.0/ppm.phar"
   sha256 "99f04aed983b901aa5107204f2c8a6311353f3db258966fd659a566302306e6e"
   version "4.2.0"
-
   bottle :unneeded
-
   
 
   def install
-    bin.install "ppm.phar" => "ppm"
+    bin.install "ppm.phar" => "ppm.phar"
+    bin.install_symlink  "#{bin}/ppm.phar" => "ppm"
   end
 
   test do
