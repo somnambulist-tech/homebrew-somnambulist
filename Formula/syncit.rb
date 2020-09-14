f1f574b88e6278822449d171a9d184379127d9f2
Dave Redfern <dave-redfern@users.noreply.github.com>
2020-09-14 17:12:31 +0000

    Updating brew formulas

--- Formula/syncit.rb
+++ Formula/syncit.rb
@@ -2,9 +2,9 @@
 class Syncit < Formula
   desc "SyncIt for Mutagen a PHP helper for managing mutagen sessions"
   homepage "https://github.com/somnambulist-tech/sync-it"
-  url "https://github.com/somnambulist-tech/sync-it/releases/download/1.2.1/mutagen-sync-it.phar"
-  sha256 "350e165d86400d0e2f4e304c9e83784a49b386231f96d35cc032b872d5a728a1"
-  version "1.2.1"
+  url "https://github.com/somnambulist-tech/sync-it/releases/download/1.2.2/mutagen-sync-it.phar"
+  sha256 "55ecc456692d9e508e89c09147101635a4133220f3295e69fa4f1dc8b1eeb818"
+  version "1.2.2"
   bottle :unneeded
   depends_on "mutagen-io/mutagen/mutagen"
 
@@ -14,7 +14,7 @@ class Syncit < Formula
   end
 
   test do
-    assert_match /^Sync-It with Mutagen 1.2.1$/, shell_output("syncit --version")
+    assert_match /^Sync-It with Mutagen 1.2.2$/, shell_output("syncit --version")
   end
 end
 
